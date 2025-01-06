39bef7a5278b15280f3bbd62eebe3852576cc662
Dante <beyondmind9@gmail.com>
2025-01-05 22:25:33 -0300

    ConfigModule and env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { CreateAccountController } from './controllers/create-account.controller';
+import { envSchema } from './env';
 import { PrismaServise } from './prisma/prisma.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaServise],
 })
